b545c01b9d0a24b369be0f0b79bce7888647eca8
DanVi <danicrisa@hotmail.com>
2022-06-02 21:49:38 -0300

    condicionais

---
+++ condicional-aninhada.js
@@ -0,0 +1,9 @@
+let numero=10;
+
+if (numero>10){
+  console.log(`${numero} é maior que 10`)
+} else if (numero === 10){
+  console.log(`${numero} é igual a 10`)
+} else {
+  console.log(`${numero} é menor que 10`)
+}
---
+++ desafio-condicional-aninhada2.js
@@ -0,0 +1,24 @@
+let idade = parseFloat(prompt('Digite sua idade', 4.20));
+
+if ( ){
+  console.log(`${resultado} é maior que 100`);
+  document.getElementsByClassName('res')[0].innerHTML=`${resultado} é maior que 100`;
+} else if (){
+  console.log(`Bingo! Deu 100!`)
+  document.getElementsByClassName('res')[1].innerHTML=`Bingo! Deu 100!`;
+} else if (){
+  console.log(`${resultado} é menor que 10`);
+  document.getElementsByClassName('res')[2].innerHTML=`${resultado} é menor que 10`;
+}else if (){
+
+}else if (){
+
+}else if (){
+
+}else if (){
+
+}else if (){
+
+}else if (){
+
+}
