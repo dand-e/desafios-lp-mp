3ca31b0ad8ce9783bf8566336dc92ce557a2fd67
DanVi <danicrisa@hotmail.com>
2022-06-09 22:57:18 -0300

    for, for of e for in

--- for.js
+++ for.js
@@ -4,10 +4,20 @@
 
 const notas = [5.1, 0.1, 6.2, 0.6, 7.4, 10]
 
-// notas.forEach(element => {
-//   console.log(`A nota ${indexOf} é ${notas}`)
-// });
-
 for(let j=0; j<notas.length; j++){
   console.log(`A nota ${j} é ${notas[j]}`)
+}
+
+for(const nota of notas){
+  console.log(nota)
+}
+
+const refeicoes = {
+  manha: 'cuzcuz',
+  tarde: 'feijoada',
+  noite: 'pizza'
+};
+
+for(const chave in refeicoes){
+  console.log(`No período da ${chave} a refeição foi ${refeicoes[chave]}`)
 }
