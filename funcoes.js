284153ae429d0ba93b6a42d3f30aa1ac5d2cd035
DanVi <danicrisa@hotmail.com>
2022-06-13 21:02:02 -0300

    funções e arrow function

---
+++ funcoes.js
@@ -0,0 +1,20 @@
+function soma(numero1, numero2){
+  return numero1+numero2;
+};
+
+console.log(soma(0,9));
+console.log(soma(1,8));
+
+const divisao = (numero1, numero2) => {
+  return numero1/numero2;
+};
+const subtracao = (numero1, numero2) => {
+  return numero1-numero2;
+};
+const multiplicacao = (numero1, numero2) => {
+  return numero1*numero2;
+};
+
+const somaTresNumeros = (n1, n2, n3) => n1+n2+n3;
+console.log(somaTresNumeros(1,3,5))
+console.log(somaTresNumeros(7,9,11))
